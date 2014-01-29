78f3902e75acec3ea485048b429aeca6f5112c56
OpenShift guest <quinntet@yahoo.com>
2014-01-29 11:50:08 -0500

    Programming project 1

--- pc2.cpp
+++ pc2.cpp
@@ -98,7 +98,7 @@ double launchHumanCannonball (double initialVelocity, double launchAngle)
 	// CODE HERE
 	//
 	// 1 convert launchAngle from degrees to radians [radangle = degangle * (PI/180)]
-	launchAngle = (launchAngle * (PI/180));
+	launchAngle = (launchAngle * (/180));
 	// 2 compute final horizontal/x velocity [xveloc = initialVelocity * cos(radangle)]
 	// 3 compute final vertical/y velocity [yveloc = initialVecity * sin(radangle) * -1]
 	// 4 compute time of flight [flighttime = (yveloc) * 2 / -9.8]
